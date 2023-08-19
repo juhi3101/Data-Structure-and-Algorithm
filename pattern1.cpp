7f579bbce38a1851ccba3b1704d9f43443b5621c
Juhi Singh <135611715+juhi3101@users.noreply.github.com>
2023-08-19 13:10:17 +0530

    Add files via upload

---
+++ pattern1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n ;
+  cin>>n;
+  for(int i=0;i<n;i++){
+    for(int j = 0;j<n-i-1;j++){
+      cout<<" ";
+    }
+    for(int k = 0;k<2*i+1;k++){
+      if(k==0  || k==2*i){
+        cout<<"* ";
+      }
+      else{
+        cout<<" ";
+      }
+    }
+    cout<<endl;
+  }
+  for(int i=0;i<n;i++){
+    for(int j=0;j<i;j++){
+      cout<<" ";
+    }
+    for(int k =0;k<2*n-2*i-1;k++){
+      if(k==0 || k==2*n-2*i-2){
+        cout<<"* ";
+      }
+      else{
+        cout<<" ";
+      }
+    }
+    cout<<endl;
+  }
+
+  
+
+}
